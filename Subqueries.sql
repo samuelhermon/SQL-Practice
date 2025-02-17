fce87a48950bed1ef07711baaaf12abef3d2c169
samuelhermon <145871063+samuelhermon@users.noreply.github.com>
2025-02-17 10:04:24 -0500

    Add files via upload

---
+++ Subqueries.sql
@@ -0,0 +1,32 @@
+## -- Subqueries --
+
+SELECT *
+FROM employee_demographics
+WHERE employee_id IN 
+				(SELECT employee_id
+					FROM employee_salary
+                    WHERE dept_id = 1)
+;
+
+
+SELECT first_name, salary, 
+(SELECT AVG(salary)
+FROM employee_salary)
+FROM employee_salary
+;
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender
+;
+
+SELECT AVG(max_age)
+FROM 
+(SELECT gender, 
+AVG(age) AS avg_age,
+MAX(age) AS max_age,
+MIN(age) AS min_age,
+COUNT(age)
+FROM employee_demographics
+GROUP BY gender) AS Aggregated_Table
+;
